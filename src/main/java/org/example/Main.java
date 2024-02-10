be639afb9e0c37469f7df0689e313697c3506370
Tommie Botsford <102128856+SqyZjq@users.noreply.github.com>
2024-02-10 20:47:45 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @Project: Default (Template) Project
+ * @Site:
+ * @Forum:
+ * @Copyright
+ * @Author: 师启源
+ * @Create: 2024-02-10 20:28
+ * @Desc： ${description}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/test/day01/FirstAuto.java
@@ -0,0 +1,36 @@
+package com.test.day01;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @Desc: REST-assured编写代码进行接口测试
+ */
+public class FirstAuto {
+    public static void main(String[] args) {
+        //通过REST-assured编写代码进行接口测试
+        //语法格式：三段式结构 given...when...then..
+        //given()后面一般我们是设置请求头、请求参数
+        //when() 后面一般我们是发起接口请求
+        //then() 后面一般我们可以解析获取接口的响应结果
+        //===================登录接口请求代码============================
+        given().
+                log().all().
+                header("Content-Type","application/json").
+                body("{\"principal\":\"lemon_auto\",\"credentials\":\"lemon123456\",\"appType\":3,\"loginType\":0}").
+        when().
+                post("http://mall.lemonban.com:8107/login").
+        then().
+                log().all();
+
+        //===================添加购物车接口请求代码=======================
+        given().
+                log().all().
+                header("Content-Type","application/json").
+                header("Authorization","bearere7bf770b-94d0-4122-9c2f-5f4941cc6632").
+                body("{\"basketId\":0,\"count\":1,\"prodId\":\"85\",\"shopId\":1,\"skuId\":417}").
+                when().
+                post("http://mall.lemonban.com:8107/p/shopCart/changeItem").
+                then().
+                log().all();
+    }
+}
